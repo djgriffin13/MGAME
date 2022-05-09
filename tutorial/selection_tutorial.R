8b0000ff1a4350277555dbe88ffce7ce2474761b
Daniel Griffin <danieljgriffin618@gmail.com>
2022-05-09 13:18:39 -0400

    Update Selection Tutorial

--- tutorial/selection_tutorial.R
+++ tutorial/selection_tutorial.R
@@ -1,29 +1,32 @@
-# Install and Load Library
-# Note only run install.packages("devtools")
+# Run install.packages("devtools")
 # if it is not already installed
+# this allows you to install from github directly
 install.packages("devtools")
-devtools::install_github("djgriffin13/isNet")
-library(isNet)
+
+# Install and Load MGAME package from github
+devtools::install_github("djgriffin13/MGAME")
+library(MGAME)
 
 # Load Data
-# Set path to local directory where the data is stored
+# Mut set path to local directory where the data is stored
 data = readr::read_csv("./tutorial/data/selection_data.csv")
 
 # Calculate Dissimilarity
-data$na_dissim = sqrt((data$na_source - data$na_target) ^ 2)
+data$na_sim = 5 - abs(data$na_ego - data$na_alter)
 
 # Run Model
-fit = ameMG(
+fit = mgame(
   data,
-  Y = "communication_count_t1",
-  Xego = c("na_source"),
-  Xalter = "na_target",
-  Xdyad = c("communication_count_t0", "na_dissim"),
+  Y = "info_share_t1",
+  Xego = c("na_ego"),
+  Xalter = "na_alter",
+  Xdyad = c("info_share_t0", "na_sim"),
   group = "team_ID",
   R = 2,
   nscan = 300,
   family = "nrm"
 )
+
 
 # Get Rresults
 summary(fit)
