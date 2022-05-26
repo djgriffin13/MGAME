8caa097cad87518a6ebe5633842cacd2e69bfb45
Daniel Griffin <danieljgriffin618@gmail.com>
2022-05-26 09:56:01 -0400

    Update Selection Tutorial

--- tutorial/selection_tutorial.R
+++ tutorial/selection_tutorial.R
@@ -1,31 +1,33 @@
-# Run install.packages("devtools")
-# if it is not already installed
-# this allows you to install from github directly
-install.packages("devtools")
+# Install and Load Library
 
-# Install and Load MGAME package from github
+# If devtools is not already installed
+# it allows you to install MGAME from github directly
+install.packages("devtools")
 devtools::install_github("djgriffin13/MGAME")
 library(MGAME)
 
 # Load Data
-# Mut set path to local directory where the data is stored
-data = readr::read_csv("./tutorial/data/selection_data.csv")
+# Must set path to local directory where the data is stored
+# Click: Session > Set Working Directory > Choose Directory...
+data = readr::read_csv("./data/selection_data.csv")
 
-# Calculate Dissimilarity
-data$na_sim = 5 - abs(data$na_ego - data$na_alter)
+# Calculate Relational Covariates
+data$negative_affect_sim_t0 = 5 - abs(data$negative_affect_ego_t0 - data$negative_affect_alter_t0)
 
 # Run Model
 fit = mgame(
-  data,
-  Y = "info_share_t1",
-  Xego = c("na_ego"),
-  Xalter = "na_alter",
-  Xdyad = c("info_share_t0", "na_sim"),
-  group = "team_ID",
-  R = 2,
-  nscan = 300,
-  family = "nrm"
+   data,
+   Y = "info_share_t1",
+   Xego = c("negative_affect_ego_t0"),
+   Xalter = c("negative_affect_alter_t0"),
+   Xdyad = c("info_share_t0", "negative_affect_sim_t0"),
+   group = "team_ID",
+   R = 2,
+   nscan = 3000,
+   family = "nrm",
+   group_standard = c("negative_affect_ego_t0","negative_affect_alter_t0","negative_affect_sim_t0"),
+   grand_standard = c("info_share_t0","info_share_t1"),
 )
 
-# Get Rresults
+# Get Results
 summary(fit)
